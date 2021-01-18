150ffe5183c90500163fdc77f1d3cb5e92f23542
AP <AP@mail.com>
2021-01-18 20:19:25 +0700

    Initial Commit

---
+++ Plant Data/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.txt_dir = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_new_name = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.dtp_new_date = New System.Windows.Forms.DateTimePicker()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(90, 18)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(161, 27)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Plant"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(551, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(157, 408)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'txt_dir
+        '
+        Me.txt_dir.Location = New System.Drawing.Point(291, 18)
+        Me.txt_dir.Name = "txt_dir"
+        Me.txt_dir.Size = New System.Drawing.Size(219, 27)
+        Me.txt_dir.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(516, 18)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(29, 27)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(16, 52)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(529, 272)
+        Me.RichTextBox2.TabIndex = 5
+        Me.RichTextBox2.Text = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 333)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 19)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Name"
+        '
+        'txt_new_name
+        '
+        Me.txt_new_name.Location = New System.Drawing.Point(90, 330)
+        Me.txt_new_name.Name = "txt_new_name"
+        Me.txt_new_name.Size = New System.Drawing.Size(195, 27)
+        Me.txt_new_name.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(291, 363)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(254, 27)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 366)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 19)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Date"
+        '
+        'dtp_new_date
+        '
+        Me.dtp_new_date.Location = New System.Drawing.Point(90, 360)
+        Me.dtp_new_date.Name = "dtp_new_date"
+        Me.dtp_new_date.Size = New System.Drawing.Size(195, 27)
+        Me.dtp_new_date.TabIndex = 10
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(198, 393)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 27)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Add"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(256, 18)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(29, 27)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "↻"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(458, 329)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(87, 27)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "Save"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(16, 393)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(87, 27)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "/dev/ Age"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(458, 393)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(87, 27)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Add"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(720, 430)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.dtp_new_date)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_new_name)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt_dir)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Plant Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents txt_dir As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_new_name As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents dtp_new_date As DateTimePicker
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Plant Data/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+    Dim date1 As Date
+    Dim date2 As Date
+    Dim ts As TimeSpan
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            My.Settings.dir = FolderBrowserDialog1.SelectedPath
+            txt_dir.Text = My.Settings.dir
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        newTask("Getting directory...")
+        txt_dir.Text = My.Settings.dir
+        Button3.PerformClick()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Computer.FileSystem.WriteAllText(My.Settings.dir & "\" & txt_new_name.Text & ".txt", "Plant name : " & txt_new_name.Text & vbNewLine & "Date planted : " & dtp_new_date.Value & vbNewLine & "Age : ", True)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        newTask("Reading files...")
+        ComboBox1.Items.Clear()
+        Try
+            For Each file As String In My.Computer.FileSystem.GetFiles(My.Settings.dir)
+                ComboBox1.Items.Add(My.Computer.FileSystem.GetName(file))
+            Next
+        Catch ex As Exception
+            MsgBox("Set folder for data storage", vbInformation, "Folder not set")
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        newTask("Retrieving " & ComboBox1.SelectedItem)
+        RichTextBox2.Text = My.Computer.FileSystem.ReadAllText(My.Settings.dir & "\" & ComboBox1.SelectedItem)
+        updateAge()
+    End Sub
+
+    Sub newTask(task As String)
+        RichTextBox1.AppendText(task & vbNewLine)
+        RichTextBox1.ScrollToCaret()
+    End Sub
+
+    Sub updateAge()
+        newTask("Updating...")
+        date1 = retrieveDate()
+        date2 = Date.Now()
+        ts = date2.Subtract(date1)
+        Dim lines As New List(Of String)
+        lines = RichTextBox2.Lines.ToList
+        lines(2) = "Age : " & ts.Days
+        RichTextBox2.Lines = lines.ToArray
+    End Sub
+
+    Function retrieveDate()
+        Dim lines As New List(Of String)
+        lines = RichTextBox2.Lines.ToList
+        Dim datePlanted As Date
+        datePlanted = lines(1).Substring(14)
+
+        Return datePlanted
+    End Function
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        My.Computer.FileSystem.WriteAllText(My.Settings.dir & "\" & ComboBox1.SelectedItem, RichTextBox2.Text, False)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        updateAge()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        RichTextBox2.AppendText(vbNewLine)
+        RichTextBox2.AppendText("-------------------------------------")
+        RichTextBox2.AppendText(vbNewLine)
+        RichTextBox2.AppendText(Date.Now().ToString("yyyy-mm-dd") & " | ")
+        RichTextBox2.AppendText(TextBox3.Text)
+    End Sub
+End Class
